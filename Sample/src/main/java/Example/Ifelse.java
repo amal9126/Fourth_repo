19a40a65023c65ab22a53ad336f3e5dae79537ad
AMAL RAJ <AMAL RAJ@LAPTOP-92ICR69A>
2025-04-10 11:31:08 +0530

    first commit

---
+++ Sample/src/main/java/Example/Ifelse.java
@@ -0,0 +1,17 @@
+package Example;
+
+public class Ifelse
+{
+public static void main (String args[])
+{
+int num=6;
+if(num%2==0)
+{
+System.out.println(num + " is a even number");
+}
+else
+{
+System.out.println(num + " is a odd number");
+}
+}
+}
